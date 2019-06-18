421835a1d9694e98ca7ce50da8102c75d492bbb4
pamccombs <pa.mccombs1@gmail.com>
2019-06-18 09:51:38 -0400

    create genre model

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,13 @@ class Game < ApplicationRecord
     has_many :genres
     has_many :users, through: :genres
 
+    def genre_category
+        self.try(:genre).try(:category)
+    end
+
+    def genre_category=(category)
+        genre = Genre.find_or_create_by(category: category)
+        self.genre = genre
+    end
+
 end
---
+++ db/migrate/20190618134248_create_genres.rb
@@ -0,0 +1,11 @@
+class CreateGenres < ActiveRecord::Migration[5.2]
+  def change
+    create_table :genres do |t|
+      t.integer :user_id
+      t.integer :game_id
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
