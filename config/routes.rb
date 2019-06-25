894039837e644a57ca5b6c975630630f0b2d90c4
pamccombs <pa.mccombs1@gmail.com>
2019-06-25 18:17:43 -0400

    log out fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
   resources :games do
     resources :genres, only: [:index, :show]
@@ -9,8 +7,10 @@ Rails.application.routes.draw do
 
   resources :users
   resources :genres
+  #resources :sessions, only: [:new, :create, :destroy]
 
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   post '/logout' => 'sessions#destroy'
 end
