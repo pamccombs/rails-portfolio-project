1a9e738ca670ab7097ec9737a2142758341b0240
pamccombs <pa.mccombs1@gmail.com>
2019-06-25 20:09:13 -0400

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
+ 
   resources :games do
     resources :genres, only: [:index, :show]
   end
 
   resources :users
   resources :genres
-  #resources :sessions, only: [:new, :create, :destroy]
+ 
 
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
