e455116eca951d252dd7b711589870a7d01d0105
pamccombs <pa.mccombs1@gmail.com>
2019-06-25 20:12:29 -0400

    add #search

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,12 @@
 class Genre < ApplicationRecord
     belongs_to :user
     belongs_to :game
+
+    def self.search(query)
+        if query.present?
+          where('CATEGORY like ?', "%#{query}%")
+        else
+          self.all
+        end
+    end
 end
